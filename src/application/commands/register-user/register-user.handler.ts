fa3dc6e1e7d82c6fe23963eb0707e6fa57c45002
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2025-05-23 20:22:35 -0300

    feat: handle http expections with either

--- src/application/commands/register-user/register-user.handler.ts
+++ src/application/commands/register-user/register-user.handler.ts
@@ -7,9 +7,20 @@ import { EmailValidator } from '../../../domain/contracts/validation/email-valid
 import { Hasher } from '../../../domain/contracts/security/hasher';
 import { UserAlreadyExistsError } from '../../errors/user-already-exists.error';
 import { Injectable, Inject } from '@nestjs/common';
+import { Either, left, right } from '../../../domain/shared/either';
+import { ICommandHandler } from '../../contracts/handler/command-handler.interface';
+import { InvalidEmailError } from '../../../domain/errors/invalid-email.error';
+import { InvalidPasswordError } from '../../../domain/errors/invalid-password-error';
+
+type OutputRegisterUserHandler = Either<
+    InvalidEmailError | InvalidPasswordError | UserAlreadyExistsError,
+    void
+>;
 
 @Injectable()
-export class RegisterUserHandler {
+export class RegisterUserHandler
+    implements ICommandHandler<RegisterUserCommand, OutputRegisterUserHandler>
+{
     constructor(
         @Inject('UserRepository')
         private readonly userRepository: UserRepository,
@@ -18,24 +29,38 @@ export class RegisterUserHandler {
         @Inject('Hasher') private readonly hasher: Hasher,
     ) {}
 
-    async execute(command: RegisterUserCommand): Promise<void> {
+    async execute(
+        command: RegisterUserCommand,
+    ): Promise<OutputRegisterUserHandler> {
         const { email, plain, role } = command;
 
-        const emailVO = Email.create(email, this.emailValidator);
-        const passwordVO = await Password.create(plain, this.hasher);
+        try {
+            const emailVO = Email.create(email, this.emailValidator);
+            const passwordVO = await Password.create(plain, this.hasher);
 
-        const userAlreadyExists =
-            await this.userRepository.findByEmail(emailVO);
-        if (userAlreadyExists) {
-            throw new UserAlreadyExistsError(email);
+            const exists = await this.userRepository.findByEmail(emailVO);
+            if (exists) {
+                return left(new UserAlreadyExistsError(email));
+            }
+
+            const user = User.create({
+                email: emailVO,
+                password: passwordVO,
+                role,
+            });
+            await this.userRepository.save(user);
+
+            return right(undefined);
+        } catch (error) {
+            if (
+                error instanceof InvalidEmailError ||
+                error instanceof InvalidPasswordError ||
+                error instanceof UserAlreadyExistsError
+            ) {
+                return left(error);
+            }
+
+            throw error;
         }
-
-        const user = User.create({
-            email: emailVO,
-            password: passwordVO,
-            role,
-        });
-
-        await this.userRepository.save(user);
     }
 }
--- src/nest-modules/auth/auth.controller.ts
+++ src/nest-modules/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+    BadRequestException,
+    ConflictException,
+    Controller,
+    Post,
+    Body,
+} from '@nestjs/common';
 import { RegisterUserHandler } from '../../application/commands/register-user/register-user.handler';
 import { RegisterUserCommand } from '../../application/commands/register-user/register-user.command';
+import { InvalidEmailError } from '../../domain/errors/invalid-email.error';
+import { InvalidPasswordError } from '../../domain/errors/invalid-password-error';
+import { UserAlreadyExistsError } from '../../application/errors/user-already-exists.error';
 
 @Controller('auth')
 export class AuthController {
@@ -14,8 +23,25 @@ export class AuthController {
             role: dto.role,
         };
 
-        await this.registerUserHandler.execute(command);
+        const resultOrError = await this.registerUserHandler.execute(command);
 
-        return { message: 'Usuário registrado com sucesso!' };
+        if (resultOrError.isLeft()) {
+            const error = resultOrError.value;
+
+            if (
+                error instanceof InvalidEmailError ||
+                error instanceof InvalidPasswordError
+            ) {
+                throw new BadRequestException(error.message);
+            }
+
+            if (error instanceof UserAlreadyExistsError) {
+                throw new ConflictException(error.message);
+            }
+
+            throw new BadRequestException('Unexpected error');
+        }
+
+        return { message: `User ${command.email} registered successfully.` };
     }
 }
